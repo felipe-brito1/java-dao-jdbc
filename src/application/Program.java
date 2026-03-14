333f6bab7bb9c4f0250d7a308b7bf158e12a1ec5
Felipe Brito <felipebrito0429@gmail.com>
2026-03-14 12:18:27 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 		Department db = new Department(1, "Books");
 		
 		Seller seller = new Seller (21, "Bob", "bob@gmail.com", LocalDate.of(2000, 04, 01), 3000.0, db);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
