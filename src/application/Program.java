2563960a2db599f6447724a996c411de3722ea2b
Felipe Brito <felipebrito0429@gmail.com>
2026-03-14 12:01:22 -0300

    Create Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department db = new Department(1, "Books");
-		System.out.println(db);
+		
+		Seller seller = new Seller (21, "Bob", "bob@gmail.com", LocalDate.of(2000, 04, 01), 3000.0, db);
+		
+		System.out.println(seller);
 	}
+	
 
 }
